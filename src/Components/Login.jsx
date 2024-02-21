5874c5d0b1617a5b997d65f6c0c476a518523795
Anezijane <ahnnieanezi@gmail.com>
2024-02-21 10:13:39 +0100

    install tailwindcss postcss

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -2,7 +2,20 @@ export default function Login() {
     return (
         <>
             <div>
-                <h1>Login</h1>
+                <form action="#">
+                    <h1>Login</h1>
+                    <div>
+                        <p>LogIn</p>
+                        <p>SignUp</p>
+                    </div>
+                    <input type="text" placeholder="Email Address"/><br/><br/>
+                    <input type="password" placeholder="Password"/><br/>
+                    <sub>Forgot Password?</sub><br/><br/>
+                    <button type="submit">LogIn</button>
+
+                    <p>Not a member? <a href="signup.html">SignUp now</a></p>
+                </form>
+              
             </div>
         </>
     )
