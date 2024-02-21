2f087b2c7a9a37999992461d78d4688d25371c09
Anezijane <ahnnieanezi@gmail.com>
2024-02-21 12:00:01 +0100

    add state to app.jsx}

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import React, { useState } from 'react';
 import Login from './Components/Login'
 import SignUp from './Components/SignUp'
 
 function App() {
+  const [isLoginPage, setIsLoginPage] = useState(true);
+
+  const togglePage = () => {
+    setIsLoginPage(!isLoginPage);
+  };
 
   return (
     <>
       <div>
-        <Login />
-        <SignUp />
+      {isLoginPage ? <Login /> : <SignUp />}
       </div>
     </>
   )
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,4 +1,5 @@
 export default function Login() {
+
     return (
         <>
             <div className="flex justify-center items-center mt-40 ">
