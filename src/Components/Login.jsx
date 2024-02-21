e5df8a4a6220ea7d6ed14c0e4c12fe7498d74544
Anezijane <ahnnieanezi@gmail.com>
2024-02-21 10:43:07 +0100

    add login components style

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,11 @@
 export default function Login() {
     return (
         <>
-            <div>
-                <form action="#">
-                    <h1>Login</h1>
-                    <div>
-                        <p>LogIn</p>
+            <div className="flex justify-center items-center mt-40 ">
+                <form action="#" className=" bg-white inline-block w-80 p-5 text-center rounded-lg">
+                    <h1 className="text-2xl font-bold tracking-wider">Login</h1>
+                    <div className="flex border justify-between mt-4 rounded-xl">
+                        <p className="bg-slate-900 text-white p-2 rounded-xl">LogIn</p>
                         <p>SignUp</p>
                     </div>
                     <input type="text" placeholder="Email Address"/><br/><br/>
