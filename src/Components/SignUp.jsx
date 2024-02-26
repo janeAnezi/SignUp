b318af6064fafa57c05d3f78287bc562c84d01a8
Anezijane <ahnnieanezi@gmail.com>
2024-02-26 15:31:32 +0100

    add state in signup component

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -1,15 +1,79 @@
-import { validateForm} from './Login';
-export default function SignUp({ onSwitchToLogin }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errors, setErrors] = useState({});
 
-    const formValidation = validateForm();
+export default function SignUp({ onSwitchToLogin }) {
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+    // const [errors, setErrors] = useState({});
+
+    // const validateForm = () => {
+    //     const errors = {};
+    //     if (!email) {
+    //         errors.email = 'Email is required';
+    //     } else if (!/\S+@\S+\.\S+/.test(email)) {
+    //         errors.email = 'Email address is not valid';
+    //     }
+
+    //     // check length
+    //     if(password.length < 8) {
+    //         errors.password = ('Password must be at least 8 characters long');
+    //     }
+    //     // check for uppercase letters
+    //     if(!/[A-Z]/.test(password)) {
+    //         errors.password = ('Password must contain at least one uppercase letter');
+    //     }
+    //     // check for lowercase letters
+    //     if(!/[a-z]/.test(password)) {
+    //         errors.push('Password must contain at least one lowercase letter');
+    //     }
+    //     // check for numbers
+    //     if (!/\d/.test(password)) {
+    //         errors.password = ('Password must contain at least one number');
+    //     }
+    //     // Check for special characters
+    //     if (!/[!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?]/.test(password)) {
+    //         errors.password = ('Password must contain at least one special character');
+    //     }
+  
+    //     if(Object.keys(errors).length === 0) {
+    //         // Clear errors
+    //             setErrors({});
+    //     }
+
+    //     return errors;
+    // };
    
+    // const handleChangeEmail = (e) => {
+    //     setEmail(e.target.value);
+    //     // Clear email error when typing
+    //     if (errors.email) {
+    //         setErrors(prevErrors => ({ ...prevErrors, email: '' }));
+    //     }
+    // };
+
+    //  const handleChangePassword = (e) => {
+    //     setPassword(e.target.value);
+    //     // Clear email error when typing
+    //     if (errors.password) {
+    //         setErrors(prevErrors => ({ ...prevErrors, password: '' }));
+    //     }
+    // };
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     const errors = validateForm();
+    //     if(Object.keys(errors).length === 0) {
+    //         // Clear email and password states
+    //             setEmail('');
+    //             setPassword('');
+
+    //         // submit to server here 
+    //         alert('Form submitted successfully');
+    //     } else {
+    //         setErrors(errors);
+    //     }
+    // }
     return (
         <>
             <div className="flex justify-center items-center mt-40 ">
-                <form action="#" className=" bg-white inline-block w-80 p-5 text-center rounded-lg">
+                <form className=" bg-white inline-block w-80 p-5 text-center rounded-lg">
                     <h1 className="text-2xl font-bold tracking-wider">SignUp</h1>
                     <div className="flex my-4 rounded-xl">
                         <p onClick={onSwitchToLogin} className=" p-2 rounded-xl inline-block w-36 cursor-pointer">LogIn</p>
